cbe1bc020da32175445adb31b9a93baf0fdfbf16
Ádám Kovács <vartefol@gmail.com>
2023-04-06 13:48:46 +0200

    fix: Return used tokens

--- supabase.ts
+++ supabase.ts
@@ -6,7 +6,7 @@ import { createClient, SupabaseClient } from 'https://esm.sh/@supabase/supabase-
  * @param auth Windmill resource representing Supabase credentials (project URL and API key).
  * @param token Supabase `access_token` and `refresh_token`. `expires_at` is a UNIX timestamp in seconds.
  * @param refresh If `true` then the `refresh_token` will be used to get a new `access_token` without checking expirity.
- * @returns A Supabase client
+ * @returns A Supabase client.
  */
 export async function createSupabaseClient(
   auth: Resource<'supabase'>,
@@ -18,23 +18,35 @@ export async function createSupabaseClient(
         global: { headers: { Authorization: `bearer ${token.access}` } },
       }
     : undefined
-  let client = createClient(auth.url, auth.key, headers)
+  const result = {
+    client: createClient(auth.url, auth.key, headers),
+    token,
+  }
 
   if ((refresh && token) || (token?.expires_at && token.expires_at * 1000 < Date.now())) {
-    const { data } = await client.auth.refreshSession({
+    const { data } = await result.client.auth.refreshSession({
       refresh_token: token.refresh,
     })
-    const newHeaders = data?.session?.access_token
+    if (data.session) {
+      result.token = {
+        access: data.session.access_token,
+        refresh: data.session.refresh_token,
+        expires_at: data.session.expires_in,
+      }
+    } else {
+      result.token = undefined
+    }
+    const newHeaders = data.session?.access_token
       ? {
           global: {
             headers: { Authorization: `bearer ${data.session.access_token}` },
           },
         }
       : undefined
-    client = createClient(auth.url, auth.key, newHeaders)
+    result.client = createClient(auth.url, auth.key, newHeaders)
   }
 
-  return client
+  return result
 }
 
 /**
@@ -42,21 +54,25 @@ export async function createSupabaseClient(
  * @param auth Windmill resource representing Supabase credentials (project URL and API key).
  * @param token Supabase `access_token` and `refresh_token`. `expires_at` (optional) is a UNIX timestamp in seconds.
  * @param fn A function that returns a Supabase query.
- * @returns The result of the query.
+ * @returns The result of the query appended by the used tokens.
  */
 export async function refreshAndRetryIfExpired(
   auth: Resource<'supabase'>,
   token: { access: string; refresh: string; expires_at?: number } | undefined,
   fn: (client: SupabaseClient) => Promise<{ data: any; error?: any }>
-) {
+): Promise<{
+  data: any
+  error?: any
+  token?: { access: string; refresh: string; expires_at?: number }
+}> {
   try {
-    const client = await createSupabaseClient(auth, token)
-    const query = await fn(client)
+    let supabase = await createSupabaseClient(auth, token)
+    let query = await fn(supabase.client)
     if (query.error?.code === 'PGRST301' && token) {
-      const client = await createSupabaseClient(auth, token, true)
-      return await fn(client)
+      supabase = await createSupabaseClient(auth, token, true)
+      query = await fn(supabase.client)
     }
-    return query
+    return { ...query, token: supabase.token }
   } catch (error) {
     return error
   }
